dcf6a771f67cf037c3ad98983811ed83e46833b5
tylerj <tylerjcagle@gmail.com>
2022-09-14 17:56:09 -0600

    shrink shortcuts

--- components/DefaultIcon.js
+++ components/DefaultIcon.js
@@ -1,13 +1,21 @@
+import React, { useEffect, useState } from 'react'
 import Image from "next/image";
+import { isTablet } from "../utils/agents";
 
 const DefaultIcon = (props) => {
   const src = props.icon.src;
   const href = props.icon.href;
+  const [mobile, setMobile] = useState(false);
 
   const smallSize = 20;
   const midSize = 28;
-  const largeSize = 48;
+  const largeSize = mobile ? 40 : 48;
   const size = props.small ? smallSize : props.mid ? midSize : largeSize
+
+  useEffect(() => {
+    const isMobile = isTablet();
+    setMobile(isMobile);
+  }, [])
 
   if (href) {
     return (
