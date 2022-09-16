ec7756e75343996f652c8ff13a724f1b23575b4e
tylerj <tylerjcagle@gmail.com>
2022-09-15 23:14:21 -0600

    update gold bar nft url

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
       },
       {
         source: "/pages/gold-nft",
-        destination: "https://gold-bar-nft.vercel.app/",
+        destination: "https://goldbarnft.vercel.app/",
         permanent: false,
       },
       {
