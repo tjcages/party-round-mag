15d95c5ed749e5d244417bf32f0e444e2cabf67b
tylerj <tylerjcagle@gmail.com>
2022-09-20 11:28:26 -0600

    update link

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
       },
       {
         source: "/pages/gold-nft",
-        destination: "https://goldbarnft.vercel.app/",
+        destination: "https://goldbarnft.xyz/",
         permanent: false,
       },
       {
